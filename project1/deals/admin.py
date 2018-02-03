f611dabb2585b469ed6d078b302914ebba7d6164
Juan sebastian bedoya <juansb827@gmail.com>
2018-02-03 13:23:26 -0500

    Renamed User model to Profile

--- project1/deals/admin.py
+++ project1/deals/admin.py
@@ -2,8 +2,10 @@
 from __future__ import unicode_literals
 from .models import City
 from .models import Offer
+from .models import Profile
 from django.contrib import admin
 
 # Register your models here.
 admin.site.register(City)
 admin.site.register(Offer)
+admin.site.register(Profile)
--- project1/deals/models.py
+++ project1/deals/models.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class City(models.Model):
     name = models.CharField(max_length=150)
@@ -15,13 +19,31 @@ class Offer(models.Model):
         city = models.ForeignKey(City, null=True)
         amount = models.DecimalField(max_digits=10, decimal_places=2)
 
-class User(models.Model):
-    name = models.CharField(max_length=150)
-    lastname = models.CharField(max_length=150)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    #las guaname = models.CharField(max_length=150)
+    #lastname = models.CharField(max_length=150)
     img_url = models.CharField(max_length=1000)
     address = models.CharField(max_length=150)
-    email = models.CharField(max_length=150)
-    password = models.CharField(max_length=150)
+    #email = models.CharField(max_length=150)
+    #password = models.CharField(max_length=150)
     city = models.ForeignKey(City, null=True)
+
+#TODO: Agregar modelos faltantes aca
+
+
+
+
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+        if(instance.is_staff== False):
+            instance.profile.save()
 
 
