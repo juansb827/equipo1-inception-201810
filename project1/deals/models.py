6d1352c0a4c8cbfca589995d437c7e19e0f5ecb9
Juan sebastian bedoya <juansb827@gmail.com>
2018-02-02 21:01:09 -0500

    -Modelo promociones

--- project1/deals/models.py
+++ project1/deals/models.py
@@ -5,3 +5,13 @@ from django.db import models
 class City(models.Model):
     name = models.CharField(max_length=150)
     country = models.CharField(max_length=150)
+
+class Offer(models.Model):
+        name = models.CharField(max_length=150)
+        img_url = models.CharField(max_length=1000)
+        description = models.CharField(max_length=1000)
+        start_date = models.DateTimeField(blank=True)
+        end_date = models.DateTimeField(blank=True)
+        city = models.ForeignKey(City, null=True)
+        amount = models.DecimalField(max_digits=10, decimal_places=2)
+
