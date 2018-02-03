5fd4a851256867d595be7059a2691904a4edcf07
Juan sebastian bedoya <juansb827@gmail.com>
2018-02-02 21:28:30 -0500

    localhost en allowed host, registro de modelo Offer en el panel de admin

--- project1/deals/admin.py
+++ project1/deals/admin.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from .models import City
+from .models import Offer
 from django.contrib import admin
 
 # Register your models here.
 admin.site.register(City)
+admin.site.register(Offer)
