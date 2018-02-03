eb1387dd5a0eb806e766912cabc38dda75ff8bf3
Juan sebastian bedoya <juansb827@gmail.com>
2018-02-02 19:40:34 -0500

    static files heroku conf

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -123,4 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
