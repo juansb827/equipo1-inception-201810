0c17b9f41d25dd6758a6f2636994b3ff92206cd9
Juan sebastian bedoya <juansb827@gmail.com>
2018-02-04 19:26:47 -0500

    offers view

--- project1/deals/views.py
+++ project1/deals/views.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Offer
+
+
+def index(request):
+    lista_promociones=Offer.objects.all()
+    context = {'lista_promociones' : lista_promociones}
+    return render(request,'deals/index.html',context)
