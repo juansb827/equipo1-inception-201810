647e74ead6bb0652ff4d961d007403ed54d6da44
camendoza94 <ca.mendoza968@uniandes.edu.co>
2018-02-08 17:59:32 -0500

    Added bootstrap to index.

--- project1/deals/models.py
+++ project1/deals/models.py
@@ -10,12 +10,13 @@ class City(models.Model):
     name = models.CharField(max_length=150)
     country = models.CharField(max_length=150)
 
+
 class Category(models.Model):
-    name= models.CharField(max_length=150)
+    name = models.CharField(max_length=150)
 
 
 class Offer(models.Model):
-    category= models.ForeignKey(Category, null=True)
+    category = models.ForeignKey(Category, null=True)
     name = models.CharField(max_length=150)
     img_url = models.CharField(max_length=1000)
     description = models.CharField(max_length=1000)
@@ -25,40 +26,32 @@ class Offer(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
 
 
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     preferences = models.ManyToManyField(Category)
-    #las guaname = models.CharField(max_length=150)
-    #lastname = models.CharField(max_length=150)
+    # las guaname = models.CharField(max_length=150)
+    # lastname = models.CharField(max_length=150)
     img_url = models.CharField(max_length=1000)
     address = models.CharField(max_length=150)
-    #email = models.CharField(max_length=150)
-    #password = models.CharField(max_length=150)
+    # email = models.CharField(max_length=150)
+    # password = models.CharField(max_length=150)
     city = models.ForeignKey(City, null=True)
 
+
 class Comment(models.Model):
-    #id_comment
+    # id_comment
     content = models.TextField(max_length=3000)
     email_comment = models.CharField(max_length=150)
     create_date = models.DateTimeField(blank=False)
     user = models.ForeignKey(User, null=False)
-
-#TODO: Agregar modelos faltantes aca
-
-
-
-
-
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
+
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    if(instance.is_staff== False):
+    if not instance.is_staff:
         instance.profile.save()
-
-
--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'deals'
+    'deals',
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
